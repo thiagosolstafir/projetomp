078f2e2ad3887c03a4f6c8b122e96ef1ac812460
Thiago <thiagosolstafir@gmail.com>
2017-03-20 11:35:51 -0300

    alteração

--- error404.php
+++ error404.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <title>Página não encontrada</title>
-    <meta charset="UTF-8"/>
+    <meta charset="iso-8859-1"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
