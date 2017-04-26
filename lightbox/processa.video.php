54450d96af46f7ce3178cfc1465071b647df6ccc
Thiago <thiagosolstafir@gmail.com>
2017-04-26 17:03:07 -0300

    add lightbox video

---
+++ lightbox/processa.video.php
@@ -0,0 +1,17 @@
+<?php header("Content-Type: text/html; charset=ISO-8859-1", true); ?>
+
+
+<div id="top-lt" style="background:transparent;">
+
+    <a onClick="lt_scroll(0,0);" class="close"></a>
+
+</div>
+
+<div id="video-lt">
+    <div class="video-int">
+        <div class="responsive-embed">
+            <iframe width="100%" height="460" src="https://www.youtube.com/embed/mM5_T-F1Yn4" frameborder="0"
+                    allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
